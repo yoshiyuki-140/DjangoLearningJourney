4e56a739bf47b1848ad0314cc5e35932996e4a72
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-12 22:42:03 +0900

    p17まで終わった'/'に'Hello World'を表示

--- djangosnippets/urls.py
+++ djangosnippets/urls.py
@@ -17,6 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from snippets.views import top  # added
+
 urlpatterns = [
+    # my url
+    path("", top, name="top"),
+    # default url
     path('admin/', admin.site.urls),
 ]
--- snippets/tests.py
+++ snippets/tests.py
@@ -1,3 +1,31 @@
+#coding:utf-8
+
 from django.test import TestCase
 
 # Create your tests here.
+from django.http import HttpRequest
+
+# 波線でるけど、テストコードとして実行するときはmanage.pyがある所から実行されるんで正しいコードだぞ
+from snippets.views import top
+
+
+# class TopPageViewTest(TestCase):
+    # def test_top_returns_200(self):
+        # request = HttpRequest()  # リクエストオブジェクトの作成
+        # response = top(request)
+        # self.assertEqual(response.status_code, 200)
+# 
+    # def test_top_returns_expected_content(self):
+        # request = HttpRequest()  # httpオブジェクトの作成
+        # response = top(request)
+        # self.assertEqual(response.content, b"Hello World")
+
+
+class TopPageTest(TestCase):
+    def test_top_returns_200(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        response = self.client.get("/")
+        self.assertEqual(response.content, b"Hello World")
