29684691fe2aec10550c0a63bc25329e1781ad6b
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-14 14:31:27 +0900

    p33~34のテストコード通ったTestCaseのsetUpメソッドをsetupとしていたのが原因<=Uは大文字

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,27 +1,66 @@
 #coding:utf-8
 
+# v This test
+# from django.contrib.auth import get_user_model
+# from django.test import TestCase, Client, RequestFactory
+# from snippets.models import Snippet
+# from django.urls import resolve
+# from snippets.views import top, snippet_new, snippet_edit, snippet_detail
+
+
+# class CreateSnippetTest(TestCase):
+# def test_should_resolve_snippet_new(self):
+# found = resolve("/snippets/new/")
+# self.assertEqual(snippet_new, found.func)
+#
+#
+# class SnippetDetailTest(TestCase):
+# def test_should_resolve_snippet_detail(self):
+# found = resolve("/snippets/1/")
+# self.assertEqual(snippet_detail, found.func)
+#
+#
+# class EditSnippets(TestCase):
+# def test_should_resolve_snippet_detail(self):
+# found = resolve("/snippets/1/edit/")
+# self.assertEqual(snippet_edit, found.func)
+
+
+# This test
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase, Client, RequestFactory
 
-# Create your tests here.
+
 from snippets.models import Snippet
-from django.urls import resolve
-from snippets.views import top, snippet_new, snippet_edit, snippet_detail
+from snippets.views import top
+
+UserModel = get_user_model()
 
 
-class CreateSnippetTest(TestCase):
-    def test_should_resolve_snippet_new(self):
-        found = resolve("/snippets/new/")
-        self.assertEqual(snippet_new, found.func)
+class TopPageRendrSnippetsTest(TestCase):
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="top_secret_pass0001"
+        )
 
+        self.snippet = Snippet.objects.create(
+            title="title1",
+            code="print('hello')",
+            description="description1",
+            created_by=self.user,
+        )
 
-class SnippetDetailTest(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/")
-        self.assertEqual(snippet_detail, found.func)
+    def test_should_return_snippet_title(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response, self.snippet.title)
 
-
-class EditSnippets(TestCase):
-    def test_should_resolve_snippet_detail(self):
-        found = resolve("/snippets/1/edit/")
-        self.assertEqual(snippet_edit, found.func)
+    def test_should_return_username(self):
+        request = RequestFactory().get("/")
+        request.user = self.user
+        response = top(request)
+        self.assertContains(response,self.user.username)
