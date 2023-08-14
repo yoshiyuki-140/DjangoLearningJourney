603f228ce1db5322fd88cef5e227a6e7c9605dbd
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-14 12:05:58 +0900

    gitignoreの追加 & p36完了

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -2,9 +2,22 @@
 # Create your views here.
 from snippets.models import Snippet
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 def top(request):
     snippets = Snippet.objects.all()
     context = {"snippets": snippets}  # テンプレートエンジンに与えるpythonオブジェクト
     return render(request, "snippets/top.html", context)
+
+
+def snippet_new(request):
+    return HttpResponse('スニペットの登録')
+
+
+def snippet_edit(request):
+    return HttpResponse('スニペットの編集')
+
+
+def snippet_detail():
+    return HttpResponse('スニペットの詳細閲覧')
