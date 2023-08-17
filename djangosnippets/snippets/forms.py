e2c8f173d0be4cfdf9d3c9b23567c70ad8e0010a
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-17 20:37:47 +0900

    p46までテスト込みで完了

---
+++ djangosnippets/snippets/forms.py
@@ -0,0 +1,10 @@
+#coding:utf-8
+
+from django import forms
+from snippets.models import Snippet
+
+
+class SnippetForm(forms.ModelForm):
+    class Meta:
+        model = Snippet
+        fields = ('title','code','description')
--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -7,13 +7,36 @@ from snippets.models import Snippet
 from django.urls import resolve
 from snippets.views import top, snippet_new, snippet_edit, snippet_detail
 
-
-# class CreateSnippetTest(TestCase):
-#     def test_should_resolve_snippet_new(self):
-#         found = resolve("/snippets/new/")
-#         self.assertEqual(snippet_new, found.func)
-
 UserModel = get_user_model()
+
+
+class CreateSnippetTest(TestCase):
+    #    def test_should_resolve_snippet_new(self):
+    #        found = resolve("/snippets/new/")
+    #        self.assertEqual(snippet_new, found.func)
+    def setUp(self):
+        self.user = UserModel.objects.create(
+            username="test_user",
+            email="test@example.com",
+            password="secret",
+        )
+        self.client.force_login(self.user)
+
+    def test_render_creation_form(self):
+        response = self.client.get("/snippets/new/")
+        self.assertContains(response, "スニペットの登録", status_code=200)
+
+    def test_create_snippet(self):
+        data = {
+            'title':'タイトル',
+            'code':'コード',
+            'description':'解説',
+        }
+        self.client.post("/snippets/new/",data)
+        snippet = Snippet.objects.get(title='タイトル')
+        self.assertEqual('コード',snippet.code)
+        self.assertEqual('解説',snippet.description)
+
 
 class SnippetDetailTest(TestCase):
     def setUp(self):
@@ -57,20 +80,20 @@ class SnippetDetailTest(TestCase):
 #             email="test@example.com",
 #             password="top_secret_pass0001"
 #         )
-# 
+#
 #         self.snippet = Snippet.objects.create(
 #             title="title1",
 #             code="print('hello')",
 #             description="description1",
 #             created_by=self.user,
 #         )
-# 
+#
 #     def test_should_return_snippet_title(self):
 #         request = RequestFactory().get("/")
 #         request.user = self.user
 #         response = top(request)
 #         self.assertContains(response, self.snippet.title)
-# 
+#
 #     def test_should_return_username(self):
 #         request = RequestFactory().get("/")
 #         request.user = self.user
