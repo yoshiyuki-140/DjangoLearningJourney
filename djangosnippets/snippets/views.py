081db1eb79f8fc533cbcf2ebc244ac68bb91d745
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-18 16:25:43 +0900

    p56の登録画面にbootstrap5を適用するところまで完了,pygmentによる構文ハイライトは未実装,なおsnippet_editの画面にて,'登録'ボタンを押すとエラーが出る

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -32,7 +32,7 @@ def snippet_new(request: HttpRequest):
 def snippet_edit(request, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     if snippet.created_by_id != request.user.id:
-        return HttpResponseForbidden("このスニペットの変種は許可されていません")
+        return HttpResponseForbidden("このスニペットの編集は許可されていません")
 
     if request.method == "POST":
         form = SnippetForm(request, instance=snippet)
