22daec0d7011b4ac49efbeb4b7c690642795e85b
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-21 14:40:30 +0900

    p65までコード書いただけ

--- djangosnippets/accounts/urls.py
+++ djangosnippets/accounts/urls.py
@@ -1,8 +1,17 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-
+from django.views.generic import CreateView
 
 urlpatterns = [
+    path(
+        "signup/",CreateView.as_view(
+            template_name = 'accounts/signup.html',
+            form_class = UserCreationForm,
+            success_url = '/'
+        ),
+        name='signup'
+    ),
     path(
         'login/',
         LoginView.as_view(
--- djangosnippets/accounts/views.py
+++ djangosnippets/accounts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import redirect
 
 # Create your views here.
+
+
+# フォームクラスを使用した登録処理の流れ
+
+@require_http_methods(["GET", "POST"])
+def signup(request):
+    if request.method == "POST":
+        # POSTリクエストを受け取ったら form.is_validメソッドと form.saveメソッドを呼ぶ
+
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect(...)
+
+    else:
+        # GET リクエストを受け取ったらformオブジェクトを用紙して、テンプレートで表示
+        form = UserCreationForm()
+
+    return render(request, "signup.html", {'form': form})
