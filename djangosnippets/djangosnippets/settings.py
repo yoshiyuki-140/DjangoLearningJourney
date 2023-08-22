a7cc8b494d3af0ae536e9e1cc32a2e881180f612
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-08-22 17:48:01 +0900

    原因不明だけど、やっぱりもともとあるコードすニッペとの編集ができない

--- djangosnippets/djangosnippets/settings.py
+++ djangosnippets/djangosnippets/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+    
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -29,7 +29,7 @@ def snippet_new(request: HttpRequest):
 
 
 @login_required
-def snippet_edit(request, snippet_id):
+def snippet_edit(request:HttpRequest, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     if snippet.created_by_id != request.user.id:
         return HttpResponseForbidden("このスニペットの編集は許可されていません")
@@ -45,6 +45,6 @@ def snippet_edit(request, snippet_id):
     return render(request, 'snippets/snippet_edit.html', {'form': form})
 
 
-def snippet_detail(request, snippet_id):
+def snippet_detail(request:HttpRequest, snippet_id):
     snippet = get_object_or_404(Snippet, pk=snippet_id)
     return render(request, "snippets/snippet_detail.html", {'snippet': snippet})
