1e7457fcde9e37e35022fe740f668a512cc55abc
yoshiyuki-140 <c1221251@st.kanazawa-it.ac.jp>
2023-09-28 00:56:41 +0900

    I added one module for the problem of 'N+1'

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # default apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # added apps
+    "testapp.apps.TestappConfig",
 ]
 
 MIDDLEWARE = [
